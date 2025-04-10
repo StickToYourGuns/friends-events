962d48832ff3aa5cb63b3b1702c085e10a51f078
Tim <promisedtolive@gmail.com>
2025-04-11 00:35:42 +0300

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { onMounted, onUnmounted } from "vue";
+
+const preventPullToRefresh = (event) => {
+  if (window.scrollY === 0 && event.touches[0].clientY > 0) {
+    event.preventDefault(); // Блокируем "pull-to-refresh"
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("touchmove", preventPullToRefresh, { passive: false });
+});
+
+onUnmounted(() => {
+  window.removeEventListener("touchmove", preventPullToRefresh);
+});
 </script>
 
 <template>
   <router-view></router-view>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
