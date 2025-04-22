05e708debe96d97ec384e09c9b46b3136c57cdfe
SonjaKuv <kuvshinova.sonja@yandex.ru>
2025-04-22 21:22:10 +0300

    docker and laravel is ready

---
+++ src/resources/js/Pages/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p v-if="user">Привет, {{ user.name }}!</p>
+        <p v-else>Гость</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: Object,
+    }
+}
+</script>
---
+++ src/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import laravel from 'laravel-vite-plugin'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'resources/js'),
+        },
+    },
+})
